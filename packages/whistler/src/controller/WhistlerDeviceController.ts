35dab92ed3927262b9895f0e1b8e4c4e9d3d1459
Aaron Shapiro <aaron@shappiro.com>
2019-02-25 11:33:12 -0500

    controller shell

--- packages/whistler/src/controller/WhistlerDeviceController.ts
+++ packages/whistler/src/controller/WhistlerDeviceController.ts
@@ -1,3 +1,21 @@
-export class WhistlerDeviceController {
+import { DeviceController } from "@open-scanner-serial/interface";
+
+import { WhistlerCommand } from "../command/WhistlerCommand";
+import { WhistlerResponse } from "../response/WhistlerResponse";
+
+export class WhistlerDeviceController implements DeviceController<WhistlerCommand<any>, WhistlerResponse> {
+
+  public connect(): Promise<void> {
+    return null;
+  }
+
+  public disconnect(): Promise<void> {
+    return null;
+  }
+
+
+  public issueCommand(command: WhistlerCommand<any>): Promise<WhistlerResponse> {
+    return null;
+  }
 
 }
