0f1acf05e7e1532193f1d49fad9feb773688cb93
Aaron Shapiro <aaron@shappiro.com>
2019-04-26 12:38:18 -0400

    fix linking

--- packages/whistler/src/controller/WhistlerDeviceController.ts
+++ packages/whistler/src/controller/WhistlerDeviceController.ts
@@ -1,12 +1,12 @@
 import SerialPort, { PortInfo } from "serialport";
 
-// import { DeviceController } from "@open-scanner-serial/interface";
+import { DeviceController } from "@open-scanner-serial/interface";
 
 import { WhistlerCommand } from "../command/WhistlerCommand";
 import { WhistlerResponse } from "../response/WhistlerResponse";
 import { BinaryUtilities } from "../utilities/BinaryUtilities";
 
-export class WhistlerDeviceController {
+export class WhistlerDeviceController implements DeviceController<WhistlerCommand<any>, WhistlerResponse> {
 
   private portInfo: SerialPort.PortInfo;
   private deviceConnection: SerialPort;
